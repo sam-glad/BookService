9b284b2b55a116d80bb40c8b27f6310dbaa10e2b
Samuel Gladstone <gladston@umich.edu>
2015-07-26 15:55:52 -0400

    Trace EF SQL

--- BookService/Models/BookServiceContext.cs
+++ BookService/Models/BookServiceContext.cs
@@ -17,6 +17,7 @@ namespace BookService.Models
     
         public BookServiceContext() : base("name=BookServiceContext")
         {
+            Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
         }
 
         public System.Data.Entity.DbSet<BookService.Models.Author> Authors { get; set; }
